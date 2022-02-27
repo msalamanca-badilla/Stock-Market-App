5ef4b1c6ccdabd68a873a216f3d5ded6fe77e618
Marianne <m.salamanca21@live.ca>
2022-02-27 18:37:39 -0500

    add main_app and env file

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+import yfinance as yf
 # Create your views here.
--- stockmarket/settings.py
+++ stockmarket/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_m9yd8c%r=vvt*qloozhowv=yc4zn8xug(t7s*_3k^*_1)qdu'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
