4b78dc2f1d5dca8211122485569b9457434089dc
Marianne <m.salamanca21@live.ca>
2022-02-27 20:30:38 -0500

    add home page views, url and template

---
+++ main_app/urls.py
@@ -0,0 +1,15 @@
+import requests
+from django.urls import path
+from . import views
+
+headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 9cc1c059bee7ea41a3ce614958f8c9a9ed9a8133'
+        }
+requestResponse = requests.get("https://api.tiingo.com/api/test?token=9cc1c059bee7ea41a3ce614958f8c9a9ed9a8133",
+                                    headers=headers)
+print(requestResponse.json())
+
+urlpatterns = [
+    path('', views.home, name= 'home')
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-import yfinance as yf
-# Create your views here.
-# pfizer = yf.Ticker('PFE')
 
-# stockinfo = pfizer.info
+def home(request):
+  return render(request, 'home.html')
 
-# for key,value in stockinfo.items():
-#     print(key, ':', value)
-
-# symbol = pfizer.info['symbol']
-# print(symbol)
 
 
