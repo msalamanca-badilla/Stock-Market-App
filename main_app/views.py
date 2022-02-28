8afef709185e4b4d332f8f14cd0bc3cdfef94038
Marianne <m.salamanca21@live.ca>
2022-02-27 19:20:43 -0500

    add main_app to settings and add yfinance api

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 import yfinance as yf
 # Create your views here.
+# pfizer = yf.Ticker('PFE')
+
+# stockinfo = pfizer.info
+
+# for key,value in stockinfo.items():
+#     print(key, ':', value)
+
+# symbol = pfizer.info['symbol']
+# print(symbol)
+
+
